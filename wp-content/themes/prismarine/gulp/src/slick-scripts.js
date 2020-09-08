50f745409ffcdea0508b6f6e6127296a030a92e1
Andres Posada Llano <andres@verypossible.com>
2020-09-08 16:18:05 -0500

    Lint fixes

--- wp-content/themes/prismarine/gulp/src/slick-scripts.js
+++ wp-content/themes/prismarine/gulp/src/slick-scripts.js
@@ -8,14 +8,14 @@
  * @since       1.0.0
  */
 
-
-document.addEventListener("DOMContentLoaded", function ($) {
-
-	$('.carousel-r').slick({
-		infinite: true,
-		slidesToShow: 5,
-		slidesToScroll: 2,
-		dots:true,
-	});
-
-}( jQuery ));
+document.addEventListener(
+	"DOMContentLoaded",
+	(function recipientsSlick($) {
+		$(".carousel-r").slick({
+			infinite: true,
+			slidesToShow: 5,
+			slidesToScroll: 2,
+			dots: true,
+		});
+	})(jQuery)
+);
