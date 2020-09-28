962ddd04dbf1f469750f6fcbe0d1a184a50ca52e
Andres Posada Llano <andres@verypossible.com>
2020-09-28 18:05:47 -0500

    Fix contact menu item

--- wp-content/themes/prismarine/gulp/src/header-menu.js
+++ wp-content/themes/prismarine/gulp/src/header-menu.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	let isActive = false;
 	const btnClick = document.getElementsByClassName("toggleMenu")[0];
 	const bodyEl = document.getElementsByTagName("BODY")[0];
-	const menuItem = document.getElementsByClassName("bvbjNav-item")[1];
+	const menuItem = document.getElementsByClassName("contactitem")[0];
 	btnClick.addEventListener("click", function setActive() {
 		if (isActive) {
 			btnClick.classList.remove("--active");
