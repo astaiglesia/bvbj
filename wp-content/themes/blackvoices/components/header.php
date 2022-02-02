5690a50ed630d4237ca2c95ff0659e0813e2debb
Bobby Rae <r.rae.dev@gmail.com>
2022-02-01 21:46:52 -0500

    add logo fields

--- wp-content/themes/blackvoices/components/header.php
+++ wp-content/themes/blackvoices/components/header.php
@@ -1,9 +1,12 @@
+<?php
+$logo = get_field('header_logo', 'option');?>
+
 <header class="header z-150 h-e100 fixed w-full top-0" data-component="header">
+
   <div class="container flex justify-between py-10">
     <a class="header__logo" href="<?= site_url() ?>" title="home">
       <span class="sr-only">Go to home page</span>
-      <?= get_component('svg', ['icon' => 'main-logo', 'width' => 145.771, 'height' => 49.904, 'classes' => 'logo logo--main']) ?>
-      <?= get_component('image', ['image' => $logo ?? null, 'classes' => 'logo']) ?>
+      <?= get_component('image', ['image' => $logo, 'classes' => 'logo', 'wrap_classes' => 'flex', 'content' => get_field('logo_copy', 'option')]) ?>
     </a><?php
     wp_nav_menu( array(
       'menu'           => 'header-menu',
